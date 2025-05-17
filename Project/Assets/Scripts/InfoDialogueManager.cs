b9687e3b42cb9a855dc2d32894b4914394531083
esnoopy <enterisagjozi@gmail.com>
2025-05-17 19:37:35 +0300

    info_interaction_third_try

---
+++ Project/Assets/Scripts/InfoDialogueManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InfoDialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI paragraphText;
+
+    public void StartInfoDialogue(InfoDialogue infoinfo){
+
+        paragraphText.text = infoinfo.paragraph;
+    }
+    
+    public void EndInfoDialogue()
+    {
+        Debug.Log("End of conversation");
+    }
+}
+
---
+++ Project/Assets/Scripts/ShowInfo.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowInfo : MonoBehaviour
+{
+    public GameObject infoPanel;
+    private InfoDialogue infoinfo;
+    private InfoDialogueManager infomanager;
+
+    void Start()
+    {
+        infoPanel.SetActive(false);
+        Debug.Log("SET FALSE");
+        infomanager = FindFirstObjectByType<InfoDialogueManager>();
+
+        if (infomanager == null)
+        {
+            Debug.Log("InfoDialogueManager not found in scene!");
+        }
+    }
+
+    void OnTriggerEnter(Collider thing)
+    {
+        if (thing.tag == "Player")
+        {
+            Debug.Log("Player inside");
+            infoPanel.SetActive(true);
+            Debug.Log("SET True");
+            if (infoinfo != null)
+            {
+                Debug.Log("InfoInfo not set.");   
+            }
+            if (infomanager != null && infoinfo != null)
+            {
+                infomanager.StartInfoDialogue(infoinfo);
+                Debug.Log("InfoInfo not set."+infoinfo); 
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider thing)
+    {
+        if(thing.tag == "Player"){
+            Debug.Log("Player outside");
+            infoPanel.SetActive(false);
+            if (infomanager != null && infoinfo != null)
+            {
+                infomanager.EndInfoDialogue();
+            }
+        }
+    }
+}
