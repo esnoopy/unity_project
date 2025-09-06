1d7281e652ec6e7c75dfbc50e25af4cb64fdaf14
esnoopy <enterisagjozi@gmail.com>
2025-09-07 00:08:29 +0300

    Player_almost_done

--- Project/Assets/Scripts/HoverUI.cs
+++ Project/Assets/Scripts/HoverUI.cs
@@ -9,7 +9,6 @@ public class HoverUI : MonoBehaviour
     public static bool GamePaused = false;
     public GameObject bookBtn;
     public GameObject book;
-    public GameObject xBook;
     //public MonoBehaviour mouseLookScript;
     public GameObject map;
 
